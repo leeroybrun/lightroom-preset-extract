c4635a03526ebacd1edce9203034ab9b8d8d37fe
Leeroy Brun <leeroy.brun@gmail.com>
2018-02-12 17:32:21 +0100

    Extract only XMP data

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const exif = require('fast-exif');
 const path = require('path');
 const fs = require('fs');
 
-const filename = path.join(__dirname, 'testFiles', 'yeti-8.jpg');
+const filename = path.join(__dirname, 'test_files', 'yeti-8.jpg');
 
 console.log('Reading from ', filename);
 
-const { ExifTool } = require("exiftool-vendored");
+const { ExifTool } = require('exiftool-vendored');
 const exiftool = new ExifTool();
 
 exiftool
-.read(filename)
-.then((tags /*: Tags */) => {console.log(tags); exiftool.end(); })
-.catch(err => { console.error("Something terrible happened: ", err); exiftool.end(); });
-
-//exif.read(filename, true).then(console.log).catch(console.error);
+  .read(filename, ['-struct', '-xmp:all'])
+  .then((tags) => {
+    console.log(tags);
+    exiftool.end();
+  })
+  .catch(err => {
+    console.error('Something terrible happened: ', err);
+    exiftool.end();
+  });
